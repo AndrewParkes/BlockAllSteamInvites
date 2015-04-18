b969eafc32c0a9458b26eff8fb51003b62081385
Andrew Parkes <andrewparkesgunnell@gmail.com>
2015-04-18 23:56:46 +0200

    Added functionality to block only level 0 users

--- Blocks all steam invites.user.js
+++ Blocks all steam invites.user.js
@@ -3,9 +3,8 @@
 // @include      *steamcommunity.com/*/home/invites*
 // @version      0.1
 // @description  Blocks all steam users much like the ignore all steam invites
-// @updateURL    https://github.com/AndrewParkes/BlockAllSteamInvites.git
-// @downloadURL  https://github.com/AndrewParkes/BlockAllSteamInvites.git
 // @author       Andrew Parkes -Ant_Shrew-
+// @namespace 	 https://greasyfork.org/users/10599
 // ==/UserScript==
 
 /*
@@ -26,7 +25,7 @@ if (typeof(element) != 'undefined' && element !== null)
 {
 
     //adds the Block all tag next to the | Ignore all tag
-    document.getElementById('pinvites_ignoreall').innerHTML =document.getElementById('pinvites_ignoreall').innerHTML +'<span class="infoBreak" >&nbsp;&nbsp;|&nbsp;&nbsp;</span>' +'<a id="Block_All" class="linkStandard">Block All</a>';
+    document.getElementById('pinvites_ignoreall').innerHTML =document.getElementById('pinvites_ignoreall').innerHTML +'<span class="infoBreak" >&nbsp;&nbsp;|&nbsp;&nbsp;</span>' +'<a id="Block_All" class="linkStandard">Block All</a>' +'<span class="infoBreak" >&nbsp;&nbsp;|&nbsp;&nbsp;</span>' +'<a id="Block_All_level_0" class="linkStandard">Block All Level 0</a>';
 
     //adds the clickable function to Block all 
     var block = document.getElementById('Block_All');
@@ -34,6 +33,48 @@ if (typeof(element) != 'undefined' && element !== null)
     {
         block.addEventListener ("click", blockAll , false);
     }
+    
+    var blocklvl0 = document.getElementById('Block_All_level_0');
+    if (blocklvl0) 
+    {
+        blocklvl0.addEventListener ("click", blockAllLevel0 , false);
+    }
+}
+
+function blockAllLevel0(zEvent) 
+{
+    //alert("blocking lvl 0");
+
+    //gathers all the functions accept ignore and block
+    var elems = document.getElementsByClassName('linkStandard');
+    var elemslvl = document.getElementsByClassName('friendPlayerLevelNum');
+    
+    var counter=0;
+    for (var i in elems) 
+    {
+        //finds the functions that contain block
+        if((elems[i]+"").indexOf("block") !=-1)
+        {
+            //aquires the users steam id
+            var userAccount=((elems[i]+"").substr(26)).substr(0,((elems[i]+"").substr(26)).indexOf(",")-1);
+            //calles steams block function
+            //FriendAccept(userAccount , 'block');
+
+            var resBox=document.getElementById('inviterBlockIcon');
+            
+            //alert(userAccount+" "+elemslvl[counter].innerHTML);
+            if(elemslvl[counter].innerHTML==0)
+            {
+                //alert(userAccount+" "+elemslvl[counter].innerHTML);
+                
+                var userAccount=((elems[i]+"").substr(26)).substr(0,((elems[i]+"").substr(26)).indexOf(",")-1);
+                //calles steams block function
+                FriendAccept(userAccount , 'block');
+            }
+            counter++;
+        }
+    }
+    //alert("blocked");*/
 }
 
 function blockAll(zEvent) 
