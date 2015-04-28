99aa13bd05f686fe1c249ff69a1c4ff3adf143b3
AndrewParkes <andrewparkesgunnell@gmail.com>
2015-04-28 07:08:00 +0200

    Update Blocks all steam invites.user.js
    Added to block banned users and private accounts.
    Both seem to be working
    Trying to add block inactive accounts but it seems to block even active accounts.

--- Blocks all steam invites.user.js
+++ Blocks all steam invites.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Blocks all steam invites
 // @include      *steamcommunity.com/*/home/invites*
-// @version      0.5
+// @version      0.6
 // @description  Blocks all steam users much like the ignore all steam invites
 // @author       Andrew Parkes -Ant_Shrew-
 // @namespace    https://greasyfork.org/users/10599
@@ -18,9 +18,12 @@ if (typeof(element) != 'undefined' && element !== null)
 
     //adds the Block all tag next to the | Ignore all tag
     document.getElementById('pinvites_ignoreall').innerHTML =document.getElementById('pinvites_ignoreall').innerHTML + '<span class="infoBreak" >&nbsp;&nbsp;|&nbsp;&nbsp;</span>' +'<a id="Block_All" class="linkStandard">Block Private</a>';
+    document.getElementById('pinvites_ignoreall').innerHTML =document.getElementById('pinvites_ignoreall').innerHTML + '<span class="infoBreak" >&nbsp;&nbsp;|&nbsp;&nbsp;</span>' +'<a id="Block_Banned" class="linkStandard">Block Banned</a>';
     document.getElementById('pinvites_ignoreall').innerHTML =document.getElementById('pinvites_ignoreall').innerHTML + '<span class="infoBreak" >&nbsp;&nbsp;|&nbsp;&nbsp;</span>' +'<a id="Block_All_level_0" class="linkStandard">Block Lvl </a>' + '<input type="text" id="inputLevel" value="0" style="width:23px;" onkeypress="return event.charCode >= 48 && event.charCode <= 57" maxlength="3">';
     document.getElementById('pinvites_ignoreall').innerHTML =document.getElementById('pinvites_ignoreall').innerHTML +'<a id="donate" title="Show your appreciation for the script" href="https://github.com/AndrewParkes/BlockAllSteamInvites/blob/master/README.md#donations" target="_blank" style="align: right; float: right;"> Donate&nbsp;&nbsp;&nbsp;&nbsp;</a>';
     document.getElementById('pinvites_ignoreall').innerHTML =document.getElementById('pinvites_ignoreall').innerHTML +'<a style="align: right; float: right; color: red;">&hearts;&nbsp;</a>';
+    
+    //document.getElementById('pinvites_ignoreall').innerHTML =document.getElementById('pinvites_ignoreall').innerHTML + '<span class="infoBreak" >&nbsp;&nbsp;|&nbsp;&nbsp;</span>' +'<a id="Block_Inactive" class="linkStandard">Block Inactive</a>';
     
     //adds the clickable function to Block all 
     var block = document.getElementById('Block_All');
@@ -34,6 +37,19 @@ if (typeof(element) != 'undefined' && element !== null)
     {
         blocklvl0.addEventListener ("click", blockAllLevel0 , false);
     }
+    
+    var blockbanned = document.getElementById('Block_Banned');
+    if (blockbanned) 
+    {
+        blockbanned.addEventListener ("click", blockbannedAcc , false);
+    }
+    
+    var blockinact = document.getElementById('Block_Inactive');
+    if (blockinact) 
+    {
+        blockinact.addEventListener ("click", blockInactive , false);
+    }
+
 }
 
 function blockAllLevel0(zEvent) 
@@ -60,8 +76,6 @@ function blockAllLevel0(zEvent)
                 //calles steams block function
                 FriendAccept(userAccount , 'block');
             }
-            
-            counter++;
         }
     }
 }
@@ -71,14 +85,13 @@ function blockPrivate(zEvent)
 {
     var xmlhttp = new XMLHttpRequest(); 
     var elems = document.getElementsByClassName('linkStandard');
-    var counter=0;
     for (var i in elems) 
     {
         
         //finds the functions that contain block
             if((elems[i]+"").indexOf("block") !=-1)
             {
-                if(parseInt(elemslvl[counter].innerHTML)<=0)
+                //if(parseInt(elemslvl[counter].innerHTML)<=0)
                 {
                 //aquires the users steam id
                 var userAccount=((elems[i]+"").substr(26)).substr(0,((elems[i]+"").substr(26)).indexOf(",")-1);
@@ -90,9 +103,61 @@ function blockPrivate(zEvent)
                 {
                     FriendAccept(userAccount , 'block');
                 }
-                counter++;
             }
         }
     }
 }
+
+function blockbannedAcc(zEvent) 
+{
+    var xmlhttp = new XMLHttpRequest(); 
+    var elems = document.getElementsByClassName('linkStandard');
+    for (var i in elems) 
+    {
+        
+        //finds the functions that contain block
+        if((elems[i]+"").indexOf("block") !=-1)
+        {
+            //aquires the users steam id
+            var userAccount=((elems[i]+"").substr(26)).substr(0,((elems[i]+"").substr(26)).indexOf(",")-1);
+            //calles steams block function
+            var url ='http://steamcommunity.com/profiles/' +userAccount+ '?xml=1'
+            xmlhttp.open("GET",url,false);
+            xmlhttp.send();
+            var text=xmlhttp.responseText;
+            if(!(text.indexOf("<vacBanned>0</vacBanned>")>-1 && text.indexOf("<tradeBanState>None</tradeBanState>")>-1 && text.indexOf("<isLimitedAccount>0</isLimitedAccount>")>-1))
+            {
+                FriendAccept(userAccount , 'block');
+            }
+        }
+    }
+}
+/*
+function blockInactive(zEvent) 
+{
+    var xmlhttp = new XMLHttpRequest(); 
+    var elems = document.getElementsByClassName('linkStandard');
+    for (var i in elems) 
+    {
+        
+        //finds the functions that contain block
+            if((elems[i]+"").indexOf("block") !=-1)
+            {
+                //if(parseInt(elemslvl[counter].innerHTML)<=0)
+                {
+                //aquires the users steam id
+                var userAccount=((elems[i]+"").substr(26)).substr(0,((elems[i]+"").substr(26)).indexOf(",")-1);
+                //calles steams block function
+                var url ='http://steamcommunity.com/profiles/' +userAccount+ '?xml=1'
+                xmlhttp.open("GET",url,false);
+                xmlhttp.send();
+                if(xmlhttp.responseText.indexOf("<hoursPlayed2Wk>0.0</hoursPlayed2Wk>")>-1)
+                {
+                    FriendAccept(userAccount , 'block');
+                }
+            }
+        }
+    }
+}
+*/
 
